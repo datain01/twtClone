dc914f94ff108ab78166da06ed259765c9ec1b46
[Soeun] <[spark1@neowiz.com]>
2023-08-16 14:05:06 +0900

    환경변수 추가수정

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     domains: ["www.gravatar.com", 
     "localhost",
-    "ec2-43-201-65-69.ap-northeast-2.compute.amazonaws.com"],
+    "ec2-43-201-114-236.ap-northeast-2.compute.amazonaws.com/"],
   },
 }
 
